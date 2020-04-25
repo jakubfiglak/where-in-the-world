ee408c2993ff0c5b57ad8a813684d67af060b26c
Jakub Figlak <jakub.figlak@gmail.com>
2020-04-25 14:34:56 +0200

    Set up project environment, create basic theme and global style

---
+++ src/components/Test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function Test() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="#" />
+        </li>
+        <li>
+          <a href="#" />
+        </li>
+        <li>
+          <a href="#" />
+        </li>
+        <li>
+          <a href="#" />
+        </li>
+        <li>
+          <a href="#" />
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/theme/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+  borderRadius: '5px',
+
+  colors: {
+    main: 'cyan',
+    secondary: 'magenta',
+  },
+};
